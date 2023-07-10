8b71c637275575df50973a8beb6a1aef57bed4d0
Lucif3r-in <ashutosh123rath@gmail.com>
2023-07-10 15:23:10 +0530

    fix: remove check

---
+++ src/pages/CodeSnippet.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { saveAs } from "file-saver";
+import html2canvas from "html2canvas";
+import { Code, CodeTitle, Menu } from "../CodeIde";
+
+const CodeSnippet = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [code, setCode] =
+    useState(`const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<h1>Hello, world!</h1>);`);
+
+  const downloadCode = () => {
+    setIsLoading(true);
+    const codeNode = document.querySelector(".code-node");
+    const parentNode = document.querySelector(".parent-node");
+    parentNode.classList.add("opacity-0");
+    codeNode.classList.add("downloading-code");
+
+    html2canvas(codeNode, {
+      allowTaint: true,
+      useCORS: true,
+      scale: 2,
+      width: codeNode.offsetWidth,
+      height: codeNode.offsetHeight,
+      dpi: 300,
+    })
+      .then((canvas) => {
+        const png = canvas.toDataURL("image/png");
+        saveAs(png, "code.png");
+        setIsLoading(false);
+        parentNode.classList.remove("opacity-0");
+        codeNode.classList.remove("downloading-code");
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+        parentNode.classList.remove("opacity-0");
+        codeNode.classList.remove("downloading-code");
+      });
+  };
+
+  return (
+    <main>
+      <Menu
+        code={code}
+        setCode={setCode}
+        downloadCode={downloadCode}
+        isLoading={isLoading}
+      />
+      <div className="parent-node">
+        <div className="code-node">
+          <div className="code">
+            <CodeTitle />
+            <Code code={code} setCode={setCode} />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default CodeSnippet;
